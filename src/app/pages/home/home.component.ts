d50c61914054f27084f3050db57c9dc84a4bcbb8
5udeepKS <sudeepkanchibailu@gmail.com>
2023-05-04 00:34:15 +0530

    Added external API's to service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(private readonly service: MovieApiServiceService) {}
 
+  bannerApiData: any = [];
+
+  ngOnInit() {
+    this.bannerData();
+  }
+
+  // banner Data
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result);
+      this.bannerApiData = result.results;
+    });
+  }
 }
--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieApiServiceService {
+  constructor(private readonly http: HttpClient) {}
 
-  constructor() { }
+  baseUrl = 'https://api.themoviedb.org/3';
+  apiKey = '08cc33bd5ae3a747598ce2ad84376e66';
+
+  // Banner Api data
+
+  bannerApiData(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/trending/all/week?api_key=${this.apiKey}`
+    );
+  }
 }
